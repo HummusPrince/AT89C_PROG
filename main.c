9f2b93dc6bf39475ec370570b6e0358fa1be97d8
HummusPrince <HummusPrince@dev.null>
2022-01-22 15:30:08 +0200

    Spits a counter

--- main.c
+++ main.c
@@ -2,8 +2,9 @@
 #include <stdint.h>
 
 #define F_OSC 12000000L
-#define BAUDRATE 2400
-#define T1_DIV 256-F_OSC/12/32/BAUDRATE
+#define BAUDRATE 4800
+#define T1_DIV 256-F_OSC/6/32/BAUDRATE
+#define SMOD0 0x40
 
 void delay(uint16_t count){
     for(volatile uint16_t i  = 0; i < count; i++){}
@@ -11,15 +12,17 @@ void delay(uint16_t count){
 
 void main(){
     
+    uint8_t i = 0;
+
     SM1 = 1;
-    PCON |= SMOD;
+    PCON |= SMOD | SMOD0;
     TMOD |= T1_M1;
     TH1 = T1_DIV;
     TR1 = 1;
 
     while(1){
-        delay(8192);
+        delay(128);
         P2_0^=1;
-        SBUF = 'U';
+        SBUF = i++;
     }
 }
